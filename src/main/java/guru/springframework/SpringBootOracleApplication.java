9dcaf746712aab8e4770f919a3579acb8588535e
mfarid <mohd.farid@devfactory.com>
2017-02-26 00:17:30 +0530

    oracle db settings

---
+++ src/main/java/guru/springframework/SpringBootOracleApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOracleApplication.class, args);
+	}
+}
